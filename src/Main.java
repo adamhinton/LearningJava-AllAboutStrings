ebbb3cc4fcae8ba637dc9226c979719c49c2e3f9
adamd <adam.d.hinton@gmail.com>
2024-02-23 22:39:30 -0500

    isEmpty()

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        printInformation("Hello world");
+        printInformation("");
 
     }
 
     public static void printInformation(String string){
+        // Interesting, length() is a function in java, as opposed to JS's number length property
         int length = string.length();
+        if (string.isEmpty()) {
+            System.out.println("String is empty");
+            return;
+        }
+
         System.out.printf("Length = %d %n", length);
         System.out.printf("First char = %c %n", string.charAt(0));
+        System.out.printf("Last char = %c %n", string.charAt(length -1));
+
+        // If string is empty, length is 0
     }
 }
