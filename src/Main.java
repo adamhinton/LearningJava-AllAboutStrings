3e2647366c0bfd2b300be62da0719b98d1728968
adamd <adam.d.hinton@gmail.com>
2024-02-23 22:44:02 -0500

    indexOf()

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,30 @@ public class Main {
 
         printInformation("Hello world");
         printInformation("");
+        printInformation("\t \n");
 
+        String helloWorld = "Hello World";
+        System.out.printf("Index of r = %d %n", helloWorld.indexOf('r'));
+        System.out.printf("Index of World = %d %n", helloWorld.indexOf("World"));
     }
 
     public static void printInformation(String string){
         // Interesting, length() is a function in java, as opposed to JS's number length property
         int length = string.length();
+
+        System.out.printf("Length = %d %n", length);
+
+        // Is empty is a "" string literal
         if (string.isEmpty()) {
             System.out.println("String is empty");
             return;
         }
 
-        System.out.printf("Length = %d %n", length);
+        // isBlank can have white spaces, blank lines and so on
+        if(string.isBlank()){
+            System.out.println("String is blank");
+        }
+
         System.out.printf("First char = %c %n", string.charAt(0));
         System.out.printf("Last char = %c %n", string.charAt(length -1));
 
